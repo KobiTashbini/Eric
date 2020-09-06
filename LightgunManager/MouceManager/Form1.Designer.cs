c111a88b6a2e8c5f4593544bd37873f226ed643a
kobi <kobits6@gmail.com>
2020-09-07 01:45:52 +0300

    init new repository LightgunManager

---
+++ LightgunManager/MouceManager/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace MouceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.X_RIGHT_LIMIT = new System.Windows.Forms.Label();
+            this.Y_UP_LIMIT = new System.Windows.Forms.Label();
+            this.Y_DOWN_LIMIT = new System.Windows.Forms.Label();
+            this.XSensitivity = new System.Windows.Forms.Label();
+            this.YSensitivityLabel = new System.Windows.Forms.Label();
+            this.RestorNormal = new System.Windows.Forms.Button();
+            this.GlobalMessage = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.X_LEFT_LIMIT = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "→";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "←";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(142, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "↑";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(142, 269);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "↓";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(502, 212);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "↑";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(502, 269);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "↓";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(600, 212);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "↑";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(600, 269);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "↓";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X_RIGHT_LIMIT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Y_UP_LIMIT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(516, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "XSensitivity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(614, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "YSensitivity";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(142, 54);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "↓";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(142, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "↑";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(139, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Y_DOWN_LIMIT";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(0, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 0;
+            // 
+            // X_RIGHT_LIMIT
+            // 
+            this.X_RIGHT_LIMIT.AutoSize = true;
+            this.X_RIGHT_LIMIT.Location = new System.Drawing.Point(324, 152);
+            this.X_RIGHT_LIMIT.Name = "X_RIGHT_LIMIT";
+            this.X_RIGHT_LIMIT.Size = new System.Drawing.Size(13, 13);
+            this.X_RIGHT_LIMIT.TabIndex = 16;
+            this.X_RIGHT_LIMIT.Text = "0";
+            // 
+            // Y_UP_LIMIT
+            // 
+            this.Y_UP_LIMIT.AutoSize = true;
+            this.Y_UP_LIMIT.Location = new System.Drawing.Point(168, 295);
+            this.Y_UP_LIMIT.Name = "Y_UP_LIMIT";
+            this.Y_UP_LIMIT.Size = new System.Drawing.Size(13, 13);
+            this.Y_UP_LIMIT.TabIndex = 17;
+            this.Y_UP_LIMIT.Text = "0";
+            // 
+            // Y_DOWN_LIMIT
+            // 
+            this.Y_DOWN_LIMIT.AutoSize = true;
+            this.Y_DOWN_LIMIT.Location = new System.Drawing.Point(168, 80);
+            this.Y_DOWN_LIMIT.Name = "Y_DOWN_LIMIT";
+            this.Y_DOWN_LIMIT.Size = new System.Drawing.Size(13, 13);
+            this.Y_DOWN_LIMIT.TabIndex = 18;
+            this.Y_DOWN_LIMIT.Text = "0";
+            // 
+            // XSensitivity
+            // 
+            this.XSensitivity.AutoSize = true;
+            this.XSensitivity.Location = new System.Drawing.Point(534, 295);
+            this.XSensitivity.Name = "XSensitivity";
+            this.XSensitivity.Size = new System.Drawing.Size(13, 13);
+            this.XSensitivity.TabIndex = 19;
+            this.XSensitivity.Text = "0";
+            // 
+            // YSensitivityLabel
+            // 
+            this.YSensitivityLabel.AutoSize = true;
+            this.YSensitivityLabel.Location = new System.Drawing.Point(631, 295);
+            this.YSensitivityLabel.Name = "YSensitivityLabel";
+            this.YSensitivityLabel.Size = new System.Drawing.Size(13, 13);
+            this.YSensitivityLabel.TabIndex = 20;
+            this.YSensitivityLabel.Text = "0";
+            // 
+            // RestorNormal
+            // 
+            this.RestorNormal.Location = new System.Drawing.Point(313, 415);
+            this.RestorNormal.Name = "RestorNormal";
+            this.RestorNormal.Size = new System.Drawing.Size(116, 23);
+            this.RestorNormal.TabIndex = 21;
+            this.RestorNormal.Text = "Restore to normal";
+            this.RestorNormal.UseVisualStyleBackColor = true;
+            this.RestorNormal.Click += new System.EventHandler(this.RestorNormal_Click);
+            // 
+            // GlobalMessage
+            // 
+            this.GlobalMessage.AutoSize = true;
+            this.GlobalMessage.Location = new System.Drawing.Point(53, 317);
+            this.GlobalMessage.Name = "GlobalMessage";
+            this.GlobalMessage.Size = new System.Drawing.Size(0, 13);
+            this.GlobalMessage.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(38, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "X_LEFT_LIMIT";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 121);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(16, 23);
+            this.button11.TabIndex = 23;
+            this.button11.Text = "←";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(117, 121);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(19, 18);
+            this.button12.TabIndex = 24;
+            this.button12.Text = "→";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // X_LEFT_LIMIT
+            // 
+            this.X_LEFT_LIMIT.AutoSize = true;
+            this.X_LEFT_LIMIT.Location = new System.Drawing.Point(65, 151);
+            this.X_LEFT_LIMIT.Name = "X_LEFT_LIMIT";
+            this.X_LEFT_LIMIT.Size = new System.Drawing.Size(13, 13);
+            this.X_LEFT_LIMIT.TabIndex = 25;
+            this.X_LEFT_LIMIT.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.X_LEFT_LIMIT);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.GlobalMessage);
+            this.Controls.Add(this.RestorNormal);
+            this.Controls.Add(this.YSensitivityLabel);
+            this.Controls.Add(this.XSensitivity);
+            this.Controls.Add(this.Y_DOWN_LIMIT);
+            this.Controls.Add(this.Y_UP_LIMIT);
+            this.Controls.Add(this.X_RIGHT_LIMIT);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label X_RIGHT_LIMIT;
+        private System.Windows.Forms.Label Y_UP_LIMIT;
+        private System.Windows.Forms.Label Y_DOWN_LIMIT;
+        private System.Windows.Forms.Label XSensitivity;
+        private System.Windows.Forms.Label YSensitivityLabel;
+        private System.Windows.Forms.Button RestorNormal;
+        private System.Windows.Forms.Label GlobalMessage;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label X_LEFT_LIMIT;
+    }
+}
+
---
+++ LightgunManager/MouceManager/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouceManager
+{
+    public partial class Form1 : Form
+    {
+        Hashtable OutputHash = new Hashtable();
+        static SerialPort dd;
+        string arduinoReturnedValue = "NA";
+        static string tempPort = ConfigurationManager.AppSettings.Get("comNumber");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            ManagementObjectCollection mbsList = null;
+            ManagementObjectSearcher mbs = new ManagementObjectSearcher("Select * From Win32_USBHub");
+            mbsList = mbs.Get();
+
+            foreach (ManagementObject mo in mbsList)
+            {
+                Console.WriteLine("USBHub device Friendly name:{0}", mo["Name"].ToString());
+            }
+
+            string[] portNames = SerialPort.GetPortNames();
+            dd = new SerialPort();
+
+            dd.BaudRate = 9600;
+            dd.DtrEnable = true;
+            dd.PortName = "COM" + tempPort.ToString();
+            dd.Open();
+            int dddd = dd.GetHashCode();
+            // dd.DataReceived += 
+
+
+        }
+
+
+
+
+        static string port_DataReceived()
+        {
+         //   for (int i = 0; i < (10000 * dd.BytesToRead) / dd.BaudRate; i++)
+      //          ;       //Delay a bit for the serial to catch up
+          //  System.Threading.Thread.Sleep(200);
+                   //        string result = dd.ReadExisting();
+          //  if (result == null || result == "")
+         //   {
+            //    for (int i = 0; i < (10000 * dd.BytesToRead) / dd.BaudRate; i++)
+      //              ;       //Delay a bit for the serial to catch up
+             //   System.Threading.Thread.Sleep(500);
+            ////         result = dd.ReadExisting();
+            //   }
+            //    if (result == null || result == "")
+            //    {
+            //       for (int i = 0; i < (10000 * dd.BytesToRead) / dd.BaudRate; i++)
+            //     
+            string result = "NA";
+            try
+            {
+             //   dd.ReadTimeout = 1000;
+                 result = dd.ReadLine();
+            }
+            catch (Exception e)
+            {
+
+                result = "EX";
+            };       //Delay a bit for the serial to catch up
+         
+       //     }
+
+          
+
+            if (result == null)
+            {
+                result = "null";
+            }
+            if (result == "")
+            {
+                result = "empty";
+            }
+
+            //   string result = "NAAA";
+            return result;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dd.Write("X");
+            arduinoReturnedValue = port_DataReceived();
+            X_RIGHT_LIMIT.Text = arduinoReturnedValue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dd.Write("x");
+            arduinoReturnedValue = port_DataReceived();
+            X_RIGHT_LIMIT.Text = arduinoReturnedValue;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dd.Write("Y");
+            arduinoReturnedValue = port_DataReceived();
+            Y_UP_LIMIT.Text = arduinoReturnedValue;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dd.Write("y");
+            arduinoReturnedValue = port_DataReceived();
+            Y_UP_LIMIT.Text = arduinoReturnedValue;
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dd.Write("W");
+            arduinoReturnedValue = port_DataReceived();
+            XSensitivity.Text = arduinoReturnedValue;
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            dd.Write("w");
+            arduinoReturnedValue = port_DataReceived();
+            XSensitivity.Text = arduinoReturnedValue;
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            dd.Write("H");
+            arduinoReturnedValue = port_DataReceived();
+            YSensitivityLabel.Text = arduinoReturnedValue;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            dd.Write("h");
+            arduinoReturnedValue = port_DataReceived();
+            YSensitivityLabel.Text = arduinoReturnedValue;
+
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            dd.Write("Z");
+            arduinoReturnedValue = port_DataReceived();
+
+            Y_DOWN_LIMIT.Text = arduinoReturnedValue;
+
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            dd.Write("z");
+            arduinoReturnedValue = port_DataReceived();
+            Y_DOWN_LIMIT.Text = arduinoReturnedValue;
+
+        }
+
+        private void RestorNormal_Click(object sender, EventArgs e)
+        {
+            dd.Write("R");
+            arduinoReturnedValue = port_DataReceived();
+            GlobalMessage.Text = arduinoReturnedValue;
+
+            X_RIGHT_LIMIT.Text = "857";
+            X_LEFT_LIMIT.Text = "219";
+            Y_UP_LIMIT.Text = "893";
+            Y_DOWN_LIMIT.Text = "195";
+            XSensitivity.Text = "218";
+            YSensitivityLabel.Text = "209";
+           
+           
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            dd.Write("w");
+            arduinoReturnedValue = port_DataReceived();
+            X_LEFT_LIMIT.Text = arduinoReturnedValue;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            dd.Write("W");
+            arduinoReturnedValue = port_DataReceived();
+            X_LEFT_LIMIT.Text = arduinoReturnedValue;
+        }
+    }
+}
